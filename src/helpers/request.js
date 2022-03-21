5aba4bb4955305c6f6964385fc2292c64b553ee4
ak <123@qq.com>
2022-03-21 11:38:30 +0800

    baseURL切换

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
+import baseURLConfig from './config-baseURL'
+
+console.log(baseURLConfig)
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.baseURL = 'http://note-server.hunger-valley.com'
+axios.defaults.baseURL = baseURLConfig.baseURL
 axios.defaults.withCredentials = true //允许跨域
 
 export default function request(url, type = 'GET', data = {}) {
