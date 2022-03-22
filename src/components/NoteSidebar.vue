4c329fb2bb78ed4883ee39e14a8c5a8f7ca83779
ak <123@qq.com>
2022-03-22 20:52:57 +0800

    笔记详情笔记本列表切换功能

--- src/components/NoteSidebar.vue
+++ src/components/NoteSidebar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="note-sidebar">
-    <span class="btn add-note" >添加笔记</span>
-    <el-dropdown class="notebook-title"  @command="handleCommand" placement="bottom">
+    <span class="btn add-note">添加笔记</span>
+    <el-dropdown class="notebook-title" @command="handleCommand" placement="bottom">
       <span class="el-dropdown-link">
-        我的笔记本1 <i class="iconfont icon-down"></i>
+        {{ curBook.title}}<i class="iconfont icon-down"></i>
       </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item v-for="notebook in notebooks" :command="notebook.id">{{notebook.title}}</el-dropdown-item>
-        <el-dropdown-item  command="trash">回收站</el-dropdown-item>
+        <el-dropdown-item v-for="notebook in notebooks" :command="notebook.id">{{ notebook.title }}</el-dropdown-item>
+        <el-dropdown-item command="trash">回收站</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
     <div class="menu">
@@ -16,9 +16,9 @@
     </div>
     <ul class="notes">
       <li v-for="note in notes">
-        <router-link :to="`/note?noteId=${note.id}`">
-          <span class="date">{{note.updatedAtFriendly}}</span>
-          <span class="title">{{note.title}}</span>
+        <router-link :to="`/note?noteId=${note.id}&notebookId=${curBook.id}`">
+          <span class="date">{{ note.updatedAtFriendly }}</span>
+          <span class="title">{{ note.title }}</span>
         </router-link>
       </li>
     </ul>
@@ -35,34 +35,29 @@ export default {
     Notebooks.getAll()
       .then(res => {
         this.notebooks = res.data
-      })
+        this.curBook = this.notebooks.find(notebook => notebook.id === this.$route.query.notebookId) || this.notebooks[0] || {}
+        return Notes.getAll({notebookId: this.curBook.id})
+      }).then(res => {
+      this.notes = res.data
+    })
   },
   data() {
     return {
       notebooks: [],
-      notes:[
-        {
-          id: 11,
-          title: '第1个笔记',
-          updatedAtFriendly: '刚刚'
-        },
-        {
-          id: 12,
-          title: '第2个笔记',
-          updatedAtFriendly: '3分钟前'
-        },
-      ]
+      notes: [],
+      curBook: {}
     }
   },
 
   methods: {
     handleCommand(notebookId) {
-      if(notebookId !== 'trash') {
-        Notes.getAll({ notebookId })
-          .then(res => {
-            this.notes = res.data
-          })
+      if (notebookId === 'trash') {
+        return this.$router.push({path: '/trash'})
       }
+      Notes.getAll({notebookId})
+        .then(res => {
+          this.notes = res.data
+        })
     }
 
   }
@@ -71,7 +66,7 @@ export default {
 </script>
 
 
-<style lang="less" >
+<style lang="less">
 @import url(../assets/css/note-sidebar.less);
 
 </style>
