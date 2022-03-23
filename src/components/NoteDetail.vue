76c10fd5bfe34d1ff0a6110ba2654728cd9c312f
ak <123@qq.com>
2022-03-23 11:40:35 +0800

    支持markdown语法

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -3,20 +3,22 @@
     <note-sidebar @update:notes="val => notes = val"></note-sidebar>
     <div class="note-detail">
       <div class="note-empty" v-show="!curNote.id">请选择笔记</div>
-      <div v-show="curNote.id">
+      <div class="note-detail-ct" v-show="curNote.id">
         <div class="note-bar">
           <span> 创建日期: {{ curNote.createdAtFriendly }}</span>
           <span> 更新日期: {{ curNote.updatedAtFriendly }}</span>
           <span> {{ statusText }}</span>
           <span class="iconfont icon-delete" @click="deleteNote"></span>
-          <span class="iconfont icon-fullscreen"></span>
+          <span class="iconfont icon-fullscreen" @click="isShowPreview=!isShowPreview"></span>
         </div>
         <div class="note-title">
-          <input type="text" v-model:value="curNote.title" @input="updateNote" @keydown="statusText='正在输入'" placeholder="输入标题">
+          <input type="text" v-model:value="curNote.title" @input="updateNote" @keydown="statusText='正在输入'"
+                 placeholder="输入标题">
         </div>
         <div class="editor">
-          <textarea v-show="true" v-model:value="curNote.content" @input="updateNote" @keydown="statusText='正在输入'" placeholder="输入内容, 支持 markdown 语法"></textarea>
-          <div class="preview markdown-body" v-html="" v-show="false"></div>
+          <textarea v-show="isShowPreview" v-model:value="curNote.content" @input="updateNote"
+                    @keydown="statusText='正在输入'" placeholder="输入内容, 支持 markdown 语法"></textarea>
+          <div class="preview markdown-body" v-html="previewContent" v-show="!isShowPreview"></div>
         </div>
       </div>
 
@@ -32,6 +34,9 @@ import NoteSidebar from '@/components/NoteSidebar'
 import Bus from '@/helpers/bus'
 import _ from 'lodash'
 import Notes from '@/apis/notes'
+import MarkdownIt from 'markdown-it'
+
+let md = new MarkdownIt()
 
 export default {
   components: {
@@ -43,9 +48,15 @@ export default {
       curNote: {},
       notes: [],
       statusText: '笔记未修改',
+      isShowPreview: false
     }
   },
-
+  computed: {
+    previewContent() {
+      console.log(this.curNote.content || '')
+      return md.render(this.curNote.content || '')
+    }
+  },
   created() {
     Auth.getInfo()
       .then(res => {
